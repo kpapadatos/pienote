6ce78a06741b41f04f58388d639d4b70498fe381
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2021-08-15 16:08:53 +0300

    Unwrap webapp

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
-import Application from './classes/Application';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-Application.start();
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
